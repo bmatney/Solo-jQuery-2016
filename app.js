8e656b763ae3a54537b511316cb7c299ac77785c
Brian Matney <brianamatney@gmail.com>
2016-11-07 18:16:21 -0600

    it makes sense now

--- app.js
+++ app.js
@@ -1,21 +1,48 @@
 // Global Variables
 
-var i = 0;
+var numRed = 0;
+var numYellow = 0;
+var numGreen = 0;
+var numBlue = 0;
+var color;
 
 $(document).ready(function () {
-  console.log("It's working!");
-  $(".color-button").on("click", function(){
-    var value;
-    switch ($(".color-button").index(this)) {
-      case 0:
-        value = (".color-button").data("red");
-        break;
-      case 1:
-
-          break;
-      default:
-
-    }
+  $(".color-button").on("click", function () {
+    color = $(this).data("color");
+    colorFunction();
   });
 
 });
+
+function colorFunction() {
+  switch (color) {
+    case "red":
+      numRed++;
+      $("#red").text("Total red: " + numRed);
+      makeBox();
+      break;
+    case "yellow":
+      numYellow++;
+      $("#yellow").text("Total yellow: " + numYellow);
+      makeBox();
+      break;
+    case "green":
+      numGreen++;
+      $("#green").text("Total green: " + numGreen);
+      makeBox();
+      break;
+    case "blue":
+      numBlue++;
+      $("#blue").text("Total blue: " + numBlue);
+      makeBox();
+      break;
+  }
+}
+
+function makeBox() {
+  $(".container").append('<div class="color-cube"></div>');
+  var $el = $(".container").children().last();
+
+  $el.append("<div class='color-cube  " + color + "'> </div>");
+  $el.css("display", "inline-block");
+}
